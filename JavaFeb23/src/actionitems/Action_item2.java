388dc7f78f9bac324328d03bb4301a2d362a9e20
rajshrees23 <rajshree.saha99@gmail.com>
2023-03-04 11:51:50 -0500

    Added new classes to the package

---
+++ JavaFeb23/src/actionitems/Action_item2.java
@@ -0,0 +1,24 @@
+package actionitems;
+
+public class Action_item2 {
+
+	public static void main(String[] args) {
+		
+		String name = "Sandy"; String city = "Los Angeles."; 
+		 int val = 20 ; String college = "UCLA. "; String profession = "lawyer.";
+		String animal = "turtle"; String petname = "Munch.";
+		String shortcut = val + ", Sandy " ;
+		String result = "Once there was a person named " + name + " who lived in " + city
+		+ " At the age of "+ shortcut + " went to college at " + college
+		+ name + " graduated and went to work as a " + profession 
+		+ " Then, " + name + " adopted a " + animal + " named " + petname ; 
+
+System.out.println(result);
+		
+		
+		
+	}
+	
+	
+}
+
---
+++ JavaFeb23/src/actionitems/AddingNumbers.java
@@ -0,0 +1,35 @@
+package actionitems;
+
+public class AddingNumbers {
+
+	public static void main(String[] args) {
+
+		//FOR LOOP - SUM
+		int sum = 0;
+		for (int i = 0; i<=15; i++)
+		{
+			sum = sum + i;
+			
+		}
+		System.out.println("The resultsis: " + sum);
+		
+	
+
+		
+		//WHILE LOOP FOR SUM
+				int sum1 = 0;
+				int i = 0; 
+				while (i <=15)
+				{
+					sum1= sum1+ i; i++;
+				}
+			System.out.println("The result is :" + sum1);
+			
+			
+			
+			
+		}
+}
+		
+
+
